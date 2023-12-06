ecf825455ef00475a3decb7fa553c0174f0d69c4
SunDesignStore <sun.yang@me.com>
2023-12-06 18:06:16 +0800

    Create 2.1-2.2.py

---
+++ 2.1-2.2.py
@@ -0,0 +1,12 @@
+#2.1
+
+message = "hello World!"  # 创建一个名为 message 的变量，并赋值为字符串 "hello World!"
+print(message)  # 打印变量 message 的值到控制台
+
+#2.2
+
+message = "hello World!"  # 创建一个名为 message 的变量，并赋值为字符串 "hello Word!"
+print(message)  # 打印变量 message 的值到控制台，输出：hello Word!
+
+message = "hello python!"  # 将变量 message 的值更改为字符串 "hello python!"
+print(message)  # 再次打印变量 message 的值到控制台，输出：hello python!
