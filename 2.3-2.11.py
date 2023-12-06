581b38e72b9e95a98ffb8f79da5c1d9d50ee929f
SunDesignStore <sun.yang@me.com>
2023-12-06 19:02:41 +0800

    Create 2.3-2.11.py

---
+++ 2.3-2.11.py
@@ -0,0 +1,41 @@
+#2.3 组合
+name = "Sun" 
+print(f"我是{name}")
+
+#2.4 字母大小写 upper/较高的  lower/较低的
+name = "Sun"
+print(name.upper())
+print(name.lower())
+
+#2.5 2.6 输出包括引号
+say = "我想学python"
+print(f'我说“{say}”')
+
+#2.7 \t制表符 \n换行 rstrip()去空格
+print("我说\n\t你\n\t我\n\t他")
+
+name = "Sun "
+print(f'{name.rstrip()}yang')
+
+#2.8 数字
+print(2+6)
+print(2*4)
+print(24/3)
+print(10-2)
+
+#2.9 变量数字
+number = 8
+print(f"我喜欢{number}") 
+
+#多数赋值
+s,u,n = 1,2,3
+print(f"{s}\n{u}\n{n}")
+
+#2.10 注释
+"""
+这是一个
+多行注释
+"""
+
+#2.11 python之禅
+import this
